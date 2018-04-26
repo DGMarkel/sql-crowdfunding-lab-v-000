d463c2b52d23c6b772f9dbca38090bf53f837ae8
David Markel <davidgmarkel@gmail.com>
2018-04-26 17:30:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,15 +42,16 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   ON pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY sum(pledges.amount);
-  "  
+  "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  SELECT projects.category, pledges.amount
+  "SELECT projects.category, pledges.amount
   FROM projects
   JOIN pledges
   ON pledges.project_id = projects.id
-  WHERE projects.category = "music"
+  WHERE projects.category = "music";
+  "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
