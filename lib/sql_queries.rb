15a9fa52c1f418429265febc6ab171efd5821fd4
David Markel <davidgmarkel@gmail.com>
2018-04-26 17:29:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,21 +31,26 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   LEFT JOIN pledges
   ON pledges.project_id = projects.id
-  GROUP BY projects.title HAVING diff >= 0
+  GROUP BY projects.title HAVING diff >= 0;
   "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  SELECT users.name, sum(pledges.amount)
+  "SELECT users.name, sum(pledges.amount)
   FROM users
   LEFT JOIN pledges
   ON pledges.user_id = users.id
-  GROUP BY users.name 
-  ORDER BY sum(pledges.amount)  
+  GROUP BY users.name
+  ORDER BY sum(pledges.amount);
+  "  
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  SELECT projects.category, pledges.amount
+  FROM projects
+  JOIN pledges
+  ON pledges.project_id = projects.id
+  WHERE projects.category = "music"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
