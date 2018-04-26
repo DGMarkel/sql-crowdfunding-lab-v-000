cb4f5c1576b5026d626939bf49a344231a87d130
David Markel <davidgmarkel@gmail.com>
2018-04-26 16:06:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,12 +12,18 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
  LEFT OUTER JOIN pledges
  ON pledges.project_id = projects.id
  GROUP BY projects.title
- ORDER BY title ASC;
+ ORDER BY projects.title ASC;
  "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  SELECT users.name, sum(pledges.amount)
+  FROM users
+  LEFT OUTER JOIN pledges
+  ON pledges.project_id = users.id
+  GROUP BY users.name
+  ORDER BY users.name ASC;
+  
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
