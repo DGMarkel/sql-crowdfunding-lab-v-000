dd3de73c42a832e2d1b89b81aef47007e7559b11
David Markel <davidgmarkel@gmail.com>
2018-04-26 16:56:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   SELECT projects.title, sum(pledges.amount) - sum(projects.funding_goal) 
   FROM projects
-  INNER JOIN pledges
+  OUTER JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY projects.title
 
