f129c970490c626774b736e9f7174114ec104d69
David Markel <davidgmarkel@gmail.com>
2018-04-26 17:17:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,11 +36,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  SELECT users.name, sum(pledges.amount) 
+  SELECT users.name, sum(pledges.amount)
   FROM users
-  JOIN pledges
+  LEFT JOIN pledges
   ON pledges.user_id = users.id
-  GROUP BY sum(pledges.amount)  
+  GROUP BY users.name HAVING sum(pledges.amount)  
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
